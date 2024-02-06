b24bbf53c25805b660f451ae62f975c0143c274e
c3618282 <g.francois8777@student.leedsbeckett.ac.uk>
2024-02-06 15:18:27 +0000

    Week 1 Commits

---
+++ WeeklyExercises/1GettingStarted/PersonalDetails.java
@@ -0,0 +1,8 @@
+public class PersonalDetails {
+    public static void main(String[] args) {
+        System.out.println("Name: Guillaume Francois");
+        System.out.println("Address: 56 Anale Avenue");
+        System.out.println("Age: 22");
+        System.out.println("Mobile: 0790124112");
+    }
+}
---
+++ WeeklyExercises/2DataAndExpressions/Table_Of_Students_Grades.java
@@ -0,0 +1,13 @@
+public class Table_Of_Students_Grades {
+    public static void main(String[] args) {
+        System.out.println("///////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        System.out.println("==          Student Points          ==");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\///////////////////");
+        System.out.println(" ");
+        System.out.println("Name            Lab     Bonus   Total");
+        System.out.println("----            ---     -----   -----");
+        System.out.println("Tom             83      5       88");
+        System.out.println("Jerry           64      8       72");
+
+    }
+}
